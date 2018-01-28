6610875032b612901cc483e3b74984a0465a6116
yestodorrow <power998@163.com>
2018-01-28 20:09:57 +0800

    add files

---
+++ 20180127/nostopview.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var url = require('url');
+var l="http://res.app.corpautohome.com/html/annual_party_2018/index.html?flag=yyi2qi&from=timeline&isappinstalled=0";
+
+
+
+setInterval(function(){
+    l fresh()
+},1)
